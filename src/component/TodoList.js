d6e254b94c309504cbbad00f6e67238e94b49528
KieranFowler <kieran9fowler@gmail.com>
2025-07-02 12:48:05 +0100

    add form to allow the user to add Todo, include add handleSubmit, and handleChange to onChange, create button for form submission, move to do list display code to the bottom

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -5,9 +5,8 @@ import {connect} from "react-redux";
 
 class TodoList extends Component {
   state = {
-    newTodo: ""
+    newTodo: "",
   };
-
 
   // When the component mounts, fetch existing todos from the store
   componentDidMount() {
@@ -16,28 +15,45 @@ class TodoList extends Component {
 
   // Update local state as user types into the input field
   handleChange = (e) => {
-    this.setState({newTodo: e.target.value});
-  }
+    this.setState({ newTodo: e.target.value });
+  };
   // Handle form submission to add a new todo
-    handleSubmit = (e) => {
-      e.preventDefault();
-      const { newTodo } = this.state;
-      if (newTodo.trim()) {
-        // Dispatch the addTodo action
-        this.props.addTodo({ task: newTodo.trim() });
-        this.setState({ newTodo: "" }); // Clear input after adding
-      }
-    };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { newTodo } = this.state;
+    if (newTodo.trim()) {
+      // Dispatch the addTodo action
+      this.props.addTodo({ task: newTodo.trim() });
+      this.setState({ newTodo: "" });
+    }
+  };
 
   render() {
-    const {todos} = this.props.data;
-    return (<ul className="todo-list">
-      {todos && todos.length
-        ? todos.map((todo, index) => {
-          return <Todo key={`todo-${index}`} todo={todo.task}/>;
-        })
-        : "No todos, yay!"}
-    </ul>);
+    const { todos } = this.props.data;
+
+    return (
+      <>
+        {/* The Form to add a new todo to the list */}
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="You can Add a new todo here"
+            value={this.state.newTodo}
+            onChange={this.handleChange}
+          />
+          <button type="submit">Add</button>
+        </form>
+
+        {/* This is the List of Todo code */}
+        <ul className="todo-list">
+          {todos && todos.length > 0
+            ? todos.map((todo, index) => (
+                <Todo key={`todo-${index}`} todo={todo.task} />
+              ))
+            : "No todos, yay!"}
+        </ul>
+      </>
+    );
   }
 }
 
